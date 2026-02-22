240c3ca7393b5b3ee460c3a665cdc2df1f60db3e
Lucky Rathore <lucky57.iitd@gmail.com>
2026-02-22 15:41:47 +0000

    add backend env variable

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,7 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export const MAX_RETRIES = 5;
 export const INITIAL_DELAY_MS = 1000;
-export const BACKEND_URL = "http://localhost:8080";
+export const BACKEND_URL = process.env.BACKEND_URL;
