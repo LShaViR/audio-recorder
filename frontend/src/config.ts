fe2790523da2d2eec960f18099199fefcf46ffb3
Lucky Rathore <lucky57.iitd@gmail.com>
2026-02-22 16:37:27 +0000

    update setInterval in onserver

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,7 +1,3 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
 export const MAX_RETRIES = 5;
 export const INITIAL_DELAY_MS = 1000;
-export const BACKEND_URL = process.env.BACKEND_URL;
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
